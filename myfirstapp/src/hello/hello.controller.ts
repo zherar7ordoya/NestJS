41f212555a20455b540a57d008628ee95f2ab12d
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-10-16 12:56:17 -0300

    Add new routes and ValidateuserPipe to HelloController
    Introduced several new GET routes in HelloController for different status codes and parameter parsing. Added ValidateuserPipe for validating and transforming user query parameters, along with its unit test.

--- myfirstapp/src/hello/hello.controller.ts
+++ myfirstapp/src/hello/hello.controller.ts
@@ -3,8 +3,9 @@ Cuando hablamos de controladores, solamente estamos hablando
 de funciones que se van a llamar cuando una ruta sea visitada.
 */
 
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, ParseBoolPipe, ParseFilePipeBuilder, ParseIntPipe, Query, Req, Res } from '@nestjs/common';
 import type { Request, Response } from 'express';
+import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
 
 @Controller()
 export class HelloController {
@@ -12,5 +13,43 @@ export class HelloController {
     index(@Req() request: Request, @Res() response: Response) {
         console.log(request.url);
         response.status(200).json({ message: 'Hello World' });
+    }
+
+    @Get('new')
+    @HttpCode(201)
+    somethingNew() {
+        return "Something New";
+    }
+
+    @Get('notfound')
+    @HttpCode(404)
+    notFoundPage(@Req() request: Request, @Res() response: Response) {
+        //console.log(request.url);
+        //response.status(404).json({ message: 'Not Found' });
+        return "Not Found";
+    }
+
+    @Get('error')
+    @HttpCode(500)
+    errorPage() {
+        return "Internal Server Error";
+    }
+
+    @Get('ticket/:num')
+    getNumber(@Param('num', ParseIntPipe) num: number) {
+        return num + 14;
+    }
+
+    @Get('active/:status')
+    isUserActive(@Param('status', ParseBoolPipe) status: boolean) {
+        console.log(typeof status);
+        return status;
+    }
+
+    @Get('greet')
+    greet(@Query(ValidateuserPipe) query: {name: string, age: number}) {
+        console.log(typeof query.name);
+        console.log(typeof query.age);
+        return `Hello ${query.name}, you are ${query.age + 100} years old.`;
     }
 }
---
+++ myfirstapp/src/hello/pipes/validateuser/validateuser.pipe.ts
@@ -0,0 +1,16 @@
+import { ArgumentMetadata, HttpException, HttpStatus, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class ValidateuserPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    console.log('value', value);
+    
+    const ageNumber = Number(value.age);
+
+    if(isNaN(ageNumber)) {
+        throw new HttpException('Age must be a number', HttpStatus.BAD_REQUEST);
+    }
+
+    return {...value, age: ageNumber};
+  }
+}

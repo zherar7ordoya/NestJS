ea6f951cd62d39d01005aca3f9dca40581ab181a
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-10-17 19:01:31 -0300

    Add Swagger integration and API docs annotations
    Integrated @nestjs/swagger for API documentation, updated main.ts to set up Swagger UI at /api, and enabled CORS. Annotated users.controller.ts endpoints with Swagger decorators for improved API documentation. Updated dependencies in package.json to include @nestjs/swagger and @prisma/extension-accelerate.

--- myfirstapp/src/main.ts
+++ myfirstapp/src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true
+        }),
+    );
+
+    const config = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
+    app.enableCors();
+
     await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
--- myfirstapp/src/users/users.controller.ts
+++ myfirstapp/src/users/users.controller.ts
@@ -6,18 +6,25 @@ de funciones que se van a llamar cuando una ruta sea visitada.
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Usuarios')
 export class UsersController {
 
     constructor(private usersService: UsersService) { }
 
     @Get('users')
+    @ApiOperation({ summary: 'Obtener todos los usuarios' })
+    @ApiResponse({ status: 200, description: 'Lista de usuarios obtenida correctamente.' })
+    @ApiResponse({ status: 403, description: 'Forbidden.' })
     getUsers() {
         return this.usersService.getUsers();
     }
 
     @Post('users')
+    @ApiOperation({ summary: 'Crear un nuevo usuario' })
+    @ApiResponse({ status: 201, description: 'Usuario creado correctamente.' })
     createUser(@Body() user: CreateUserDto) {
         return this.usersService.createUser(user);
     }
